8aae51b27c3e1ec89aea9711ccbc2db79b006a9e
kpgusev <kpgusev@stud.kantiana.ru>
2025-09-15 18:02:50 +0200

    Format code

--- src/vulkan_triangle.cc
+++ src/vulkan_triangle.cc
@@ -9,14 +9,16 @@
 int main(int argc, char **argv) {
   glfwInit(); // TODO: RAII-wrap, `glfwSetErrorCallback`, not `GLFW_FALSE` check
 
-  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // TODO: set `GLFW_RESIZABLE`, `GLFW_DECORATED`, etc.
-  GLFWwindow *window = glfwCreateWindow(512, 512, "", nullptr, nullptr); // TODO: RAII-wrap
+  // TODO: set `GLFW_RESIZABLE`, `GLFW_DECORATED`, etc.
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  GLFWwindow *window =
+      glfwCreateWindow(512, 512, "", nullptr, nullptr); // TODO: RAII-wrap
 
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
   }
 
   glfwDestroyWindow(window); // TODO: RAII-wrap
-  glfwTerminate(); // TODO: RAII-wrap
+  glfwTerminate();           // TODO: RAII-wrap
   return EXIT_SUCCESS;
 }
